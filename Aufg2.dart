647ce7bd8ddf5be0e65b0e8ebd9b65cc7b1e019c
Brayteh <ya3koub5@gmail.com>
2025-06-30 11:13:21 +0200

    aufgabe 2 in P12

---
+++ Aufg2.dart
@@ -0,0 +1,14 @@
+abstract class DatabaseRepository {
+void createMedikament(Medikament medikament);
+List<Medikament> readAllMedikamente();
+void updateMedikament(String id, Medikament medikament);
+void deleteMedikament(String id);
+}
+
+class Medikament {
+    String id;
+    String name;
+    String dosis;
+    DateTime zeit;
+Medikament(this.id, this.name, this.dosis, this.zeit);
+}
